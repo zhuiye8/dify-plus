81d53cacdbf59312c09e3ed3a3b674eaeeecb523
famousmai <906631095@qq.com>
2025-02-07 10:49:29 +0800

    fix：合并数据库迁移文件版本，否则会导致数据库更新不了

---
+++ api/migrations/versions/2025_01_23_1053-37e5bf7a1e53_.py
@@ -0,0 +1,25 @@
+"""empty message
+
+Revision ID: 37e5bf7a1e53
+Revises: 62dd723ee92b, a91b476a53de
+Create Date: 2025-01-23 10:53:27.220375
+
+"""
+from alembic import op
+import models as models
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '37e5bf7a1e53'
+down_revision = ('62dd723ee92b', 'a91b476a53de')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
