5312c4271c88797eb66804a8a3581ee00ddc9ca2
Heavyapples <w1372988970@gmail.com>
2025-03-13 15:53:49 +0800

    fix: update

--- web/app/components/datasets/settings/form/index.tsx
+++ web/app/components/datasets/settings/form/index.tsx
@@ -307,10 +307,10 @@ const Form = () => {
               <div className={labelClass}>
                 <div>
                   <div className='text-text-secondary system-sm-semibold'>{t('datasetSettings.form.retrievalSetting.title')}</div>
-                  <div className='body-xs-regular text-text-tertiary'>
+                  {/* <div className='body-xs-regular text-text-tertiary'>
                     <a target='_blank' rel='noopener noreferrer' href='https://docs.dify.ai/guides/knowledge-base/create-knowledge-and-upload-documents#id-4-retrieval-settings' className='text-text-accent'>{t('datasetSettings.form.retrievalSetting.learnMore')}</a>
                     {t('datasetSettings.form.retrievalSetting.description')}
-                  </div>
+                  </div> */}
                 </div>
               </div>
               <div className='grow'>
