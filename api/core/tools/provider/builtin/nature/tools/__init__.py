e0f4527842e349afbeeb2144775d96ceb3411bb0
zwx@nature <1563359387@qq.com>
2025-03-17 10:14:39 +0800

    feat:update

--- api/core/tools/provider/builtin/nature/tools/__init__.py
+++ api/core/tools/provider/builtin/nature/tools/__init__.py
@@ -2,9 +2,9 @@
 Nature 文档工具函数目录初始化文件
 """
 from .document_generator import DocumentGeneratorTool
-from .excel_generator import ExcelGeneratorTool
+from .table_generator import TableGeneratorTool
 
 __all__ = [
     'DocumentGeneratorTool',
-    'ExcelGeneratorTool',
+    'TableGeneratorTool',
 ] 
